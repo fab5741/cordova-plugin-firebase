a7083f1d01d53b8df527b7085adc5ae52bd6e62f
Maigret Aurélien <dewep.net@gmail.com>
2017-03-15 10:24:43 +0100

    Grant permission on startup + iOS 10 data support

--- src/ios/AppDelegate+FirebasePlugin.m
+++ src/ios/AppDelegate+FirebasePlugin.m
@@ -2,10 +2,14 @@
 #import "FirebasePlugin.h"
 #import "Firebase.h"
 #import <objc/runtime.h>
+#import <Foundation/Foundation.h>
 
 #if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
 @import UserNotifications;
 #endif
+
+@import FirebaseInstanceID;
+@import FirebaseMessaging;
 
 // Implement UNUserNotificationCenterDelegate to receive display notification via APNS for devices
 // running iOS 10 and above. Implement FIRMessagingDelegate to receive data message via FCM for
@@ -15,15 +19,22 @@
 @end
 #endif
 
+// Copied from Apple's header in case it is missing in some cases (e.g. pre-Xcode 8 builds).
+#ifndef NSFoundationVersionNumber_iOS_9_x_Max
+#define NSFoundationVersionNumber_iOS_9_x_Max 1299
+#endif
+
 #define kApplicationInBackgroundKey @"applicationInBackground"
 
 @implementation AppDelegate (FirebasePlugin)
+
 
 + (void)load {
     Method original = class_getInstanceMethod(self, @selector(application:didFinishLaunchingWithOptions:));
     Method swizzled = class_getInstanceMethod(self, @selector(application:swizzledDidFinishLaunchingWithOptions:));
     method_exchangeImplementations(original, swizzled);
 }
+
 
 - (void)setApplicationInBackground:(NSNumber *)applicationInBackground {
     objc_setAssociatedObject(self, kApplicationInBackgroundKey, applicationInBackground, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
@@ -33,98 +44,210 @@
     return objc_getAssociatedObject(self, kApplicationInBackgroundKey);
 }
 
+
 - (BOOL)application:(UIApplication *)application swizzledDidFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     [self application:application swizzledDidFinishLaunchingWithOptions:launchOptions];
-    
+
+
+    // Register for remote notifications. This shows a permission dialog on first run, to
+    // show the dialog at a more appropriate time move this registration accordingly.
+    if (floor(NSFoundationVersionNumber) <= NSFoundationVersionNumber_iOS_7_1) {
+        // iOS 7.1 or earlier. Disable the deprecation warnings.
+        #pragma clang diagnostic push
+        #pragma clang diagnostic ignored "-Wdeprecated-declarations"
+        UIRemoteNotificationType allNotificationTypes = (UIRemoteNotificationTypeSound | UIRemoteNotificationTypeAlert | UIRemoteNotificationTypeBadge);
+        [application registerForRemoteNotificationTypes:allNotificationTypes];
+        #pragma clang diagnostic pop
+    } else {
+        // iOS 8 or later
+        // [START register_for_notifications]
+        if (floor(NSFoundationVersionNumber) <= NSFoundationVersionNumber_iOS_9_x_Max) {
+            UIUserNotificationType allNotificationTypes = (UIUserNotificationTypeSound | UIUserNotificationTypeAlert | UIUserNotificationTypeBadge);
+            UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:allNotificationTypes categories:nil];
+            [[UIApplication sharedApplication] registerUserNotificationSettings:settings];
+        } else {
+            // iOS 10 or later
+            #if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
+            // For iOS 10 display notification (sent via APNS)
+            [UNUserNotificationCenter currentNotificationCenter].delegate = self;
+            UNAuthorizationOptions authOptions = UNAuthorizationOptionAlert | UNAuthorizationOptionSound | UNAuthorizationOptionBadge;
+            [[UNUserNotificationCenter currentNotificationCenter] requestAuthorizationWithOptions:authOptions completionHandler:^(BOOL granted, NSError * _Nullable error) {}];
+
+            // For iOS 10 data message (sent via FCM)
+            [FIRMessaging messaging].remoteMessageDelegate = self;
+            #endif
+        }
+
+        [[UIApplication sharedApplication] registerForRemoteNotifications];
+        // [END register_for_notifications]
+    }
+
+    // [START configure_firebase]
     [FIRApp configure];
-    
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(tokenRefreshNotification:)
-                                                 name:kFIRInstanceIDTokenRefreshNotification object:nil];
-    
+    // [END configure_firebase]
+
+    // [START add_token_refresh_observer]
+    // Add observer for InstanceID token refresh callback.
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(tokenRefreshNotification:) name:kFIRInstanceIDTokenRefreshNotification object:nil];
+    // [END add_token_refresh_observer]
+
     self.applicationInBackground = @(YES);
-    
+
     return YES;
 }
 
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    [self connectToFcm];
-    self.applicationInBackground = @(NO);
+
+// [START receive_message]
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
+    // If you are receiving a notification message while your app is in the background,
+    // this callback will not be fired till the user taps on the notification launching the application.
+    // TODO: Handle data of notification
+
+    // Add tap field
+    NSDictionary *mutableUserInfo = [userInfo mutableCopy];
+    [mutableUserInfo setValue:self.applicationInBackground forKey:@"tap"];
+
+    // Print full message.
+    NSLog(@"application-1: %@", userInfo);
+
+    // Send notification
+    [FirebasePlugin.firebasePlugin sendNotification:mutableUserInfo];
 }
 
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    [[FIRMessaging messaging] disconnect];
-    self.applicationInBackground = @(YES);
-    NSLog(@"Disconnected from FCM");
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
+    fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
+    // If you are receiving a notification message while your app is in the background,
+    // this callback will not be fired till the user taps on the notification launching the application.
+    // TODO: Handle data of notification
+
+    // Add tap field
+    NSDictionary *mutableUserInfo = [userInfo mutableCopy];
+    [mutableUserInfo setValue:self.applicationInBackground forKey:@"tap"];
+
+    // Print full message.
+    NSLog(@"application-2: %@", userInfo);
+
+    // Send notification
+    [FirebasePlugin.firebasePlugin sendNotification:mutableUserInfo];
+
+    completionHandler(UIBackgroundFetchResultNewData);
+}
+// [END receive_message]
+
+
+// [START ios_10_message_handling]
+// Receive displayed notifications for iOS 10 devices.
+#if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
+// Handle incoming notification messages while app is in the foreground.
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center
+    willPresentNotification:(UNNotification *)notification
+    withCompletionHandler:(void (^)(UNNotificationPresentationOptions))completionHandler {
+
+    // Add tap field
+    NSDictionary *mutableUserInfo = [notification.request.content.userInfo mutableCopy];
+    [mutableUserInfo setValue:self.applicationInBackground forKey:@"tap"];
+
+    // Pring full message.
+    NSLog(@"userNotificationCenter-1:%@", mutableUserInfo);
+
+    // Send notification
+    [FirebasePlugin.firebasePlugin sendNotification:mutableUserInfo];
+
+    completionHandler(UNNotificationPresentationOptionNone);
 }
 
+// Handle notification messages after display notification is tapped by the user.
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center
+    didReceiveNotificationResponse:(UNNotificationResponse *)response
+    withCompletionHandler:(void (^)())completionHandler {
+
+    // Add tap field
+    NSDictionary *mutableUserInfo = [response.notification.request.content.userInfo mutableCopy];
+    [mutableUserInfo setValue:self.applicationInBackground forKey:@"tap"];
+
+    // Pring full message.
+    NSLog(@"userNotificationCenter-2:%@", mutableUserInfo);
+
+    // Send notification
+    [FirebasePlugin.firebasePlugin sendNotification:mutableUserInfo];
+
+    completionHandler();
+}
+#endif
+// [END ios_10_message_handling]
+
+
+// [START ios_10_data_message_handling]
+#if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
+// Receive data message on iOS 10 devices while app is in the foreground.
+- (void)applicationReceivedRemoteMessage:(FIRMessagingRemoteMessage *)remoteMessage {
+    // Add tap field
+    NSDictionary *mutableUserInfo = [remoteMessage.appData mutableCopy];
+    [mutableUserInfo setValue:self.applicationInBackground forKey:@"tap"];
+
+    // Pring full message.
+    NSLog(@"applicationReceivedRemoteMessage-1:%@", mutableUserInfo);
+
+    // Send notification
+    [FirebasePlugin.firebasePlugin sendNotification:mutableUserInfo];
+}
+#endif
+// [END ios_10_data_message_handling]
+
+
+// [START refresh_token]
 - (void)tokenRefreshNotification:(NSNotification *)notification {
     // Note that this callback will be fired everytime a new token is generated, including the first
     // time. So if you need to retrieve the token as soon as it is available this is where that
     // should be done.
     NSString *refreshedToken = [[FIRInstanceID instanceID] token];
     NSLog(@"InstanceID token: %@", refreshedToken);
-    
+
     // Connect to FCM since connection may have failed when attempted before having a token.
     [self connectToFcm];
 
+    // Send token to application server.
     [FirebasePlugin.firebasePlugin sendToken:refreshedToken];
 }
+// [END refresh_token]
 
+
+// [START connect_to_fcm]
 - (void)connectToFcm {
+    // Won't connect since there is no token
+    if (![[FIRInstanceID instanceID] token]) {
+        return;
+    }
+
+    // Disconnect previous FCM connection if it exists.
+    [[FIRMessaging messaging] disconnect];
+
     [[FIRMessaging messaging] connectWithCompletion:^(NSError * _Nullable error) {
         if (error != nil) {
             NSLog(@"Unable to connect to FCM. %@", error);
         } else {
             NSLog(@"Connected to FCM.");
-            NSString *refreshedToken = [[FIRInstanceID instanceID] token];
-            NSLog(@"InstanceID token: %@", refreshedToken);
         }
     }];
 }
+// [END connect_to_fcm]
 
-- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
-    NSDictionary *mutableUserInfo = [userInfo mutableCopy];
-    
-    [mutableUserInfo setValue:self.applicationInBackground forKey:@"tap"];
-    
-    // Pring full message.
-    NSLog(@"%@", mutableUserInfo);
-    
-    [FirebasePlugin.firebasePlugin sendNotification:mutableUserInfo];
+
+// [START connect_on_active]
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    [self connectToFcm];
+    self.applicationInBackground = @(NO);
 }
+// [END connect_on_active]
 
-- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
-    fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
 
-    NSDictionary *mutableUserInfo = [userInfo mutableCopy];
-    
-    [mutableUserInfo setValue:self.applicationInBackground forKey:@"tap"];
-    
-    // Pring full message.
-    NSLog(@"%@", mutableUserInfo);
-    
-    [FirebasePlugin.firebasePlugin sendNotification:mutableUserInfo];
+// [START disconnect_from_fcm]
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    [[FIRMessaging messaging] disconnect];
+    self.applicationInBackground = @(YES);
+    NSLog(@"Disconnected from FCM");
 }
+// [END disconnect_from_fcm]
 
-#if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
-- (void)userNotificationCenter:(UNUserNotificationCenter *)center
-       willPresentNotification:(UNNotification *)notification
-         withCompletionHandler:(void (^)(UNNotificationPresentationOptions))completionHandler {
-    NSDictionary *mutableUserInfo = [notification.request.content.userInfo mutableCopy];
-    
-    [mutableUserInfo setValue:self.applicationInBackground forKey:@"tap"];
-    
-    // Pring full message.
-    NSLog(@"%@", mutableUserInfo);
-    
-    [FirebasePlugin.firebasePlugin sendNotification:mutableUserInfo];
-}
-
-// Receive data message on iOS 10 devices.
-- (void)applicationReceivedRemoteMessage:(FIRMessagingRemoteMessage *)remoteMessage {
-    // Print full message
-    NSLog(@"%@", [remoteMessage appData]);
-}
-#endif
 
 @end
